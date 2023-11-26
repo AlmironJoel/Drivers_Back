4193456159f99a9920e5a8843b6aaa154592af24
Joel Almiron <brn_joel@hotmail.com>
2023-11-26 17:08:47 -0300

    deply

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ server.use(cors());
 
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin','*');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header(
         'Access-Control-Allow-Headers',
